bdce41e0d78de23f926f0ac54f73a69f3cdcab7c
Larry Fuller <lfullerj@gmail.com>
2018-11-17 21:01:25 -0500

    working app

---
+++ FriendFinder/app/data/friend.js
@@ -0,0 +1,89 @@
+var friendList = [
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+
+      {
+        "name":"Harry Potter",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            3,
+            2,
+            1,
+            4,
+            1,
+            1,
+            4,
+            2,
+            2,
+            5
+          ]
+      },
+
+      {
+        "name":"John Wick",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            5,
+            5,
+            4,
+            3,
+            2,
+            4,
+            1,
+            5,
+            3
+          ]
+      },
+
+      {
+        "name":"Charles Xavier",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            4,
+            2,
+            3,
+            1,
+            3,
+            3,
+            4,
+            3,
+            2,
+            4
+          ]
+      },
+
+      {
+        "name":"Hank McCoy",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+          ]
+      },
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendList;
